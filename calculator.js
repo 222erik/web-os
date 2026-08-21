c4967fedef2a97b5b8a715006cea50242caba9a5
Erik <erikolsson0@proton.me>
2026-08-21 16:17:12 +0200

    execute display

--- calculator.js
+++ calculator.js
@@ -4,6 +4,8 @@ var displayState = "type"
 var buttons = document.getElementsByClassName("calculatorButton")
 var clearButton = document.getElementById("calculatorClearButton")
 
+const operators = ["+", "-", "×", "÷"]
+
 clearButton.addEventListener("click", function() {
     display.textContent = "0"
 })
@@ -23,7 +25,7 @@ function writeDigit(digit) {
 
 function writeSymbol(symbol) {
     let operatiorMentioned = false
-    for (const op of ["+", "-", "×", "÷"]) {
+    for (const op of operators) {
         if (display.textContent.includes(op)) {
             operatiorMentioned = true
         }
@@ -34,13 +36,13 @@ function writeSymbol(symbol) {
         if (c == ".") {
             dotAllowed = false
             break
-        } else if (["+", "-", "×", "÷"].includes(c)) {
+        } else if (operators.includes(c)) {
             break
         }
     }
 
     if (displayState != "show") {
-        if (["+", "-", "×", "÷"].includes(symbol) && !operatiorMentioned) {
+        if (operators.includes(symbol) && !operatiorMentioned) {
             display.textContent = display.textContent + symbol
         } else if (symbol == "." && dotAllowed) {
             display.textContent = display.textContent + "."
@@ -49,12 +51,45 @@ function writeSymbol(symbol) {
     displayState = "type"
 }
 
+function executeDisplay() {
+    let operator = ""
+    for (const op of operators) {
+        if (display.textContent.includes(op)) {
+            operator = op
+            break
+        }
+    }
+    if (operator == "") {
+        return
+    }
+
+    let [num1, num2] = display.textContent.split(operator)
+    switch (operator) {
+        case "+":
+            display.textContent = parseFloat(num1) + parseFloat(num2)
+            break
+        case "-":
+            display.textContent = parseFloat(num1) - parseFloat(num2)
+            break
+        case "×":
+            display.textContent = parseFloat(num1) * parseFloat(num2)
+            break
+        case "÷":
+            display.textContent = parseFloat(num1) / parseFloat(num2)
+            break
+    }
+}
+
 for (const button of buttons) {
     button.addEventListener("click", function() {
         if (["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"].includes(button.textContent)) {
             writeDigit(button.textContent)
-        } else if ([".", "+", "-", "×", "÷"].includes(button.textContent)) {
+        } else if (operators.includes(button.textContent)) {
             writeSymbol(button.textContent)
+        } else if (button.textContent == "=") {
+            executeDisplay()
+        } else {
+            console.log("invalid button")
         }
     })
 }
